163f81c7e1b78802144f672e9629a99ba4d1b258
TaricOv <tareq.ouf@gmail.com>
2024-02-12 23:26:50 +0200

    added fav icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
+import { AppFormHero } from './.nuxt/components.d';
 export default ({
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt'],
     build: {
         transpile: ["@headlessui/vue"],
     },
+    head:{
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico' }
+          ],
+      },
     colorMode: {
         classSuffix: '',
     },
